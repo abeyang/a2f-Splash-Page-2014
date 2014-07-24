f3e5d7c318963ab1354d22c9e7840ef64c5f53d0
Abe Yang <abeyang@gmail.com>
2014-07-24 16:12:16 -0700

    Initial checkin
    Skeleton files

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+	// @todo Super array goes here
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,400italic' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link href="favicon.ico" rel="shortcut icon">
+
+	<!-- @todo Need analytics tracker -->
+
+	<title>Welcome to acts2fellowship</title>
+  
+</head>
+
+<body>
+
+	<!-- @todo rest of code goes here -->
+	<div class="temp">
+		Temp
+	</div>
+
+</body>
+</html>
